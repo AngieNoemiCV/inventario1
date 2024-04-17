2dda7fee6e84e45b58b54290908eb9e0310919f0
AngieNoemiCV <20460159@colima.tecnm.mx>
2024-04-17 14:07:21 -0600

    Login2

--- apps/screen/Login.tsx
+++ apps/screen/Login.tsx
@@ -1,10 +1,22 @@
-import { SafeAreaView ,StyleSheet, View, TextInput, Button, Text } from "react-native";
+import { SafeAreaView ,StyleSheet, View, TextInput, Button, Text, Alert } from "react-native";
 import React from "react";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { useState } from "react";
 
 const styles = StyleSheet.create({
+    screen:{
+        height:'100%',
+        backgroundColor:'#323844',
+        justifyContent: 'center',
+        alignItems:'center'
+    },
+
     container: {
         alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor:'#c0c0c0',
+        width:'100%',
+        padding: 16,
     },
 
     TextInput:{
@@ -18,16 +30,46 @@ const styles = StyleSheet.create({
     }
 });
 
+type RootStackParamList ={
+    Home: undefined;
+    Login: undefined;
+}
+type LoginProps={
+    navigation: StackNavigationProp<RootStackParamList,'Home'>;
+}
 
 
-function Login (): React.JSX.Element{
+
+function Login ({navigation}: LoginProps): React.JSX.Element{
+    const [usuario, setUsuario] = useState('');
+    const [contrasena, setContrasena] = useState ('');
+
+    const btnIngresarOnPress = async function () {
+        if (usuario && contrasena){
+            navigation.navigate('Home');
+            return;
+        }
+            Alert.alert('Fallido', 'Datos Incorrectos');
+    };
     return(
-        <SafeAreaView>
+        <SafeAreaView style = {styles.screen}>
             <View style={styles.container}>
                 <Text>Iniciar Sesión</Text>
-                <TextInput style={styles.TextInput} placeholder='Usuario'/>
-                <TextInput style={styles.TextInput} placeholder='Contraseña'/>
-                <Button title="Ingresar"/>
+                <TextInput 
+                style={styles.TextInput} 
+                placeholder='Usuario' 
+                placeholderTextColor='#828894'
+                onChangeText={u => setUsuario(u)}
+                />
+                <TextInput 
+                style={styles.TextInput} 
+                placeholder='Contraseña' 
+                placeholderTextColor='#828894' 
+                secureTextEntry={true}
+                onChangeText={p => setContrasena (p)}
+                />
+                <Button title="Ingresar"
+                onPress={btnIngresarOnPress}/>
             </View>
         </SafeAreaView>
     );
