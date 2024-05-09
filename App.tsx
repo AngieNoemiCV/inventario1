e39107b16c3890e8ef324099018c618bf9d96ee0
AngieNoemiCV <20460159@colima.tecnm.mx>
2024-05-09 14:44:15 -0600

    Cambios

--- App.tsx
+++ App.tsx
@@ -1,21 +1,55 @@
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationAction,
+  NavigationContainer,
+  NavigationProp,
+  useNavigation,
+} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from './apps/screen/Home';
 import Login from './apps/screen/Login';
+import ProductDetails, {Params as ProductDetailsParams} from './apps/screen/ProductDetails';
+import ProductAdd from './apps/screen/ProductAdd';
+import {Button} from 'react-native';
 
-const Stack = createStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+  Detalles: ProductDetailsParams;
+  ProductAdd: undefined;
+  Login: undefined;
+};
 
-export default function App() {
+export type StackNavigation = NavigationProp<RootStackParamList>;
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+function HomeHeader(): React.JSX.Element {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  return (
+    <Button title="Agregar" onPress={() => navigation.navigate('ProductAdd')} />
+  );
+}
+
+export default function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen 
-        name="Login" 
-        component={Login}
-        options={{headerShown: false, 
-          headerStyle:{backgroundColor:'#ffa000'},
-         }} />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+            headerStyle: {backgroundColor: '#ffa000'},
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerRight: HomeHeader,
+          }}
+        />
+        <Stack.Screen name="Detalles" component={ProductDetails} />
+        <Stack.Screen name="ProductAdd" component={ProductAdd} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- apps/screen/Home.tsx
+++ apps/screen/Home.tsx
@@ -1,12 +1,12 @@
 import { StackNavigationProp } from '@react-navigation/stack';
-import React from 'react';
-import {Button, SafeAreaView, Text} from 'react-native';
-import {RouteProp} from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { RouteProp } from '@react-navigation/native';
+import { Product } from './model/Products';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import { RootStackParamList } from '../../App';
+import LocalDB from './persistence/localbd';
 
-type RootStackParamList = {
-  Home: undefined
-
-}
 type HomeScreenProps = StackNavigationProp<RootStackParamList, 'Home'>;
 type HomeScreenRoute = RouteProp<RootStackParamList, 'Home'>;
 
@@ -15,14 +15,82 @@ type HomeProps = {
   route: HomeScreenRoute;
 };
 
+function Home({ navigation }: HomeProps): React.JSX.Element {
+  const [products, setProducts] = useState<Product[]>([]);
+  const productItem = ({ item }: { item: Product }) => (
+    <TouchableOpacity style={styles.productItem}
+      onPress={() => navigation.push("Detalles", { product: item })}>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ flexDirection: 'column', flexGrow: 9 }}>
+          <Text style={styles.itemTitle}>{item.nombre}</Text>
+          <Text style={styles.itemDetails}> Precio: $ {item.precio}</Text>
+        </View>
+        <Text style={
+          [
+            styles.itemBadge, item.currentStock < item.minStock ? styles.itemBadgeError : null,
+          ]
+        }>{item.currentStock}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
-function Home({navigation}: HomeProps): React.JSX.Element {
+  useEffect(() => {
+    LocalDB.init();
+    navigation.addListener('focus', async () => {
+      const db = await LocalDB.connect();
+      db.transaction(async tx => {
+        tx.executeSql('SELECT * FROM productos', [], (_, res) => {
+          let prods = [];
+
+          for (let i = 0; i < res.rows.length; i++) {
+            prods.push(res.rows.item(i));
+          }
+          setProducts(prods);
+        },
+          error => console.error({ error }),
+        );
+      });
+    });
+  }, [navigation]);
+
+
   return (
     <SafeAreaView>
-      <Text>Hola</Text>
-      <Button title='Navegar' onPress={() => navigation.navigate('Home')}/>
+      <FlatList
+        data={products}
+        renderItem={productItem}
+        keyExtractor={item => item.id.toString()}
+      />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  productItem: {
+    padding: 12,
+    borderBottomColor: '#c0c0c0',
+    borderBottomWidth: 1,
+    backgroundColor: 'white',
+  },
+  itemTitle: {
+    fontSize: 24,
+    color: 'black',
+    fontWeight: 'bold',
+    // textTransform: 'uppercase',
+  },
+  itemDetails: {
+    fontSize: 14,
+    opacity: 0.7,
+  },
+  itemBadge: {
+    fontSize: 24,
+    color: 'blue',
+    fontWeight: 'bold',
+    alignSelf: 'flex-end',
+  },
+  itemBadgeError: {
+    color: 'red',
+  },
+});
 
 export default Home;
